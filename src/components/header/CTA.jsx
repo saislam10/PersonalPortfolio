3b261856c84889663eb5ef0e777cb82835204980
saislam10 <saislam@davidson.edu>
2023-09-10 16:33:35 -0400

    fixed resume

--- src/components/header/CTA.jsx
+++ src/components/header/CTA.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
-// import ResumePDF from '../../assets/Resume_v2.pdf'
-import ResumeWord from '../../assets/Resume_v2.docx'
+import ResumePDF from '../../assets/Resume_v4.pdf'
+// import ResumeWord from '../../assets/Resume_v2.docx'
 export const CTA = () => {
   return (
     <div className='cta'>
         {/* <a href={ResumePDF} download className='btn'>Download Resume</a> */}
-        <a target='_blank' rel="noreferrer" href={ResumeWord} download className='btn' >Download Resume</a>
+        <a target='_blank' rel="noreferrer" href={ResumePDF} download className='btn' >Download Resume</a>
         <a target='_blank' rel="noreferrer" href='https://www.credly.com/badges/b12f19a6-43c8-4927-bb04-bc6cb9667b81/linked_in_profile' className='btn'>View Certification</a>
 
     </div>
